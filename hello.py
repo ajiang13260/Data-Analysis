80c0cf4b1a7a3b136f36949c0052d55cd9e34f28
ajiang <47351671+ajiang13260@users.noreply.github.com>
2019-10-13 14:31:05 +0800

    Initial commit

---
+++ hello.py
@@ -0,0 +1,39 @@
+# name = 'Pheobe'
+# print('hello,',name)
+# name2 = input('enter your name:')
+# print('hello',name2)
+# print('1024 * 768 =',1024 * 768)
+
+# print('I\'m OK.')
+# print('I\'m learning\npython')
+# print('\\\n\\')
+
+# print('''line1
+# ...line2
+# ...line3''')
+# print('''line1
+# line2
+# line3''')
+# print(r'''line1
+# line2
+# line3''')
+
+# print(r'''hello,\n
+# world''')
+# print('''hello,\n
+# world''')
+
+# print(True)
+# print(False)
+# print(2<3)
+# print(2>3)
+
+# print(2>3 and 6>4)
+# print(2>3 or 6>4)
+# print(not 2>3)
+
+age = 10
+if age>=18:
+    print('adult')
+else:
+    print('child')
