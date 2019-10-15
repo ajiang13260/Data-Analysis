04536bfd142cbe34ce682af363a846f9c0c4836e
ajiang <47351671+ajiang13260@users.noreply.github.com>
2019-10-15 14:01:49 +0800

    Update helloliaoxuefeng.py
    使用dict和set

--- helloliaoxuefeng.py
+++ helloliaoxuefeng.py
@@ -229,9 +229,42 @@ y = 'ABC'
 #     n = n + 1
 # print('End')
 
-n = 0
-while n < 10:
-    n = n + 1
-    if n % 2 == 0:
-        continue
-    print(n)
+# n = 0
+# while n < 10:
+#     n = n + 1
+#     if n % 2 == 0:
+#         continue
+#     print(n)
+
+# d = {'Michael':98,'Bob':66,"Gloria":95}
+# print(d['Gloria'])
+# d['Adam'] = 90
+# print (d['Adam'])
+# print(d.get('Tomas'))
+# print('Tomas' in d)
+# d.pop('Bob')
+# print(d)
+
+# s = set([1,1,2,2,4,4,3,3,5,5])
+# s.add('p')
+# # print(s)
+# y = set(['a','b','c','d','c','b','a'])
+# y.remove('d')
+# y.add(1)
+# y.add('p')
+# # print(y)
+# print(s & y)
+# print(s | y)
+
+# a = ['c','b','a']
+# a.sort()
+# print(a)
+
+# b = 'abc'
+# print(b.replace('a','A'))
+# print(b)
+
+d = {(1, 2, 3)}
+# e = {(1, [2, 3])}
+print(d)
+# print(e)
