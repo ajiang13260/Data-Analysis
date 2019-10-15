14633eb914555b3c5bda3d2e5f3a29ab988acb91
ajiang <47351671+ajiang13260@users.noreply.github.com>
2019-10-15 09:49:28 +0800

    Update helloliaoxuefeng.py
    循环

--- helloliaoxuefeng.py
+++ helloliaoxuefeng.py
@@ -171,19 +171,19 @@ y = 'ABC'
 # else:
 #     print('00前')
 
-height = 1.75
-weight = 80.5
-bmi = weight/(height**2)
-if bmi < 18.5:
-    print('过轻')
-elif bmi <= 25:
-    print('正常')
-elif bmi <= 28:
-    print('过重')
-elif bmi <=32:
-    print('肥胖')
-else:
-    print('严重肥胖')
+# height = 1.75
+# weight = 80.5
+# bmi = weight/(height**2)
+# if bmi < 18.5:
+#     print('过轻')
+# elif bmi <= 25:
+#     print('正常')
+# elif bmi <= 28:
+#     print('过重')
+# elif bmi <=32:
+#     print('肥胖')
+# else:
+#     print('严重肥胖')
 
 # 参考别人的，找到了自己的问题源于elif条件设置错误
 # height = 1.75
@@ -200,3 +200,38 @@ else:
 #     print('肥胖')
 # elif BMI > 32:
 #     print('严重肥胖')
+
+# names = ['Michael','Bob','Mike']
+# for name in names:
+#     print(name)
+
+# sum = 0
+# for x in range(101):
+#     sum = sum + x
+# print(sum)
+
+# sum = 0
+# n = 99
+# while n > 0:
+#     sum = sum + n
+#     n = n - 2
+# print(sum)
+
+# L = ['Bart', 'Lisa', 'Adam']
+# for name in L:
+#     print('Hello,' + name + '!')
+
+# n = 1
+# while n <= 100:
+#     if n >= 10:
+#         break
+#     print(n)
+#     n = n + 1
+# print('End')
+
+n = 0
+while n < 10:
+    n = n + 1
+    if n % 2 == 0:
+        continue
+    print(n)
