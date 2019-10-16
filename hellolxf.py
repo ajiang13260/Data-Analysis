72f6016863d129daef53558ddc90451e5e111e26
ajiang <47351671+ajiang13260@users.noreply.github.com>
2019-10-16 13:50:45 +0800

    Update hellolxf.py
    函数的参数（未完成）

--- hellolxf.py
+++ hellolxf.py
@@ -307,13 +307,30 @@ y = 'ABC'
 # r = move(100,100,60,math.pi/6)
 # print(r)
 
-import math
-def quadratic(a, b, c):
-    s = b**2-4*a*c
-    if a==0 or s<0 :
-        print('无解')
-    else:
-        x1 = (-b + math.sqrt(s))/(2*a)
-        x2 = (-b - math.sqrt(s))/(2*a)
-        return x1,x2
-print(quadratic(1,7,4))
+# import math
+# def quadratic(a, b, c):
+#     s = b**2-4*a*c
+#     if a==0 or s<0 :
+#         print('无解')
+#     else:
+#         x1 = (-b + math.sqrt(s))/(2*a)
+#         x2 = (-b - math.sqrt(s))/(2*a)
+#         return x1,x2
+# print(quadratic(1,7,4))
+
+# def power(x, n=2):
+#     s = 1
+#     while n > 0:
+#         n = n - 1
+#         s = s * x
+#     return s
+# print(power(5))
+
+def add_end(L = None):
+    if L is None:
+        L = []
+    L.append('END')
+    return L
+# print(add_end([1,2,3]))
+print(add_end([]))
+print(add_end)
