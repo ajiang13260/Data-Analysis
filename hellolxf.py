67275cd7d9e573b8077fd0b103bbcd3c825f1bbd
ajiang <47351671+ajiang13260@users.noreply.github.com>
2019-10-17 18:55:40 +0800

    Update hellolxf.py
    还差一点，回去做习题

--- hellolxf.py
+++ hellolxf.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
 
+# 第一个python程序
+    # 输入和输出
 # name = 'Pheobe'
 # print('hello,',name)
 # name2 = input('enter your name:')
 # print('hello',name2)
 # print('1024 * 768 =',1024 * 768)
 
+# python基础
+    # 数据类型和变量
 # print('I\'m OK.')
 # print('I\'m learning\npython')
 # print('\\\n\\')
@@ -69,6 +73,7 @@
 # Lisa!'''
 # print(n,f,s1,s2,s3,s4)
 
+    # 字符串和编码
 # print('包含中文的str')
 # print(ord('A'))
 # print(ord('中'))
@@ -113,6 +118,7 @@ y = 'ABC'
 # r = (s2-s1)/s1
 # print('%s, 成绩提升了%f%%' % ('小明', r))
 
+    # 使用list和tuple
 # classmates = ['Michael','Bob','Tracy']
 # print(classmates)
 # print(len(classmates))
@@ -147,6 +153,7 @@ y = 'ABC'
 # print(L[1][1])
 # print(L[2][2])
 
+    # 条件判断
 # age = 5
 # if age>=18:
 #     print('your name is',age)
@@ -201,6 +208,7 @@ y = 'ABC'
 # elif BMI > 32:
 #     print('严重肥胖')
 
+    # 循环
 # names = ['Michael','Bob','Mike']
 # for name in names:
 #     print(name)
@@ -236,6 +244,7 @@ y = 'ABC'
 #         continue
 #     print(n)
 
+    # 使用dict和set
 # d = {'Michael':98,'Bob':66,"Gloria":95}
 # print(d['Gloria'])
 # d['Adam'] = 90
@@ -269,6 +278,8 @@ y = 'ABC'
 # print(d)
 # print(e)
 
+# 函数
+    # 调用函数
 # print(abs(-30))
 # print(max(1,424,35))
 # print(int('123'))
@@ -280,6 +291,7 @@ y = 'ABC'
 # print(hex(n1))
 # print(hex(n2))
 
+    # 定义函数
 # def my_abs(x):
 #     if isinstance(x,(int,float)):
 #         raise TypeError('bad operand type')
@@ -318,6 +330,7 @@ y = 'ABC'
 #         return x1,x2
 # print(quadratic(1,7,4))
 
+    # 函数的参数
 # def power(x, n=2):
 #     s = 1
 #     while n > 0:
@@ -345,12 +358,66 @@ y = 'ABC'
 # nums = [1,2,3]
 # print(calc(*nums))
 
-def person(name,age,**kw):
-    print('name:',name,'age:',age,'other:',kw)
-extra = {'city':'bj','weight':'120'}
-print(person('Bob',40,**extra))
+# def person(name,age,**kw):
+#     print('name:',name,'age:',age,'other:',kw)
+# extra = {'city':'bj','weight':'120'}
+# print(person('Bob',40,**extra))
 
-def person1(name, age, **kw):
-    print('name:', name, 'age:', age, 'other:', kw)
-extra = {'city': 'Beijing', 'job': 'Engineer'}
-print(person1('Jack', 24, **extra))
+# def person1(name, age, **kw):
+#     if 'city' in kw:
+#         pass
+#     if 'job' in kw:
+#         pass
+# #     if 'height' in kw:
+# #         pass
+#     print('name:', name, 'age:', age, 'other:', kw)
+# extra = {'city': 'Beijing', 'job': 'Engineer'}
+# print(person1('Jack', 24, **extra))
+
+# def person(name, age, *, city, job):
+#     # 这种方法似乎不适合做文本的注释，如'city:'
+#     print(name,age,city,job)
+# print(person('Bob', 24, city='bj',job='writer'))
+
+
+# def person(name, age, *, city, job):
+    # print(name, age, city, job)
+# print(person('Jack', 24, city='Beijing', job='Engineer'))
+# print(person('Jack', 24))
+# 这个函数限制了必须输入定量的参数？——命名关键字参数
+# 那这样和直接规定有什么区别呢？根本就不变化了啊
+# print(person('Jack', 24, city='Beijing', job='Engineer'))
+
+# 寻求更多种解决方法
+# def person(name, age, *, city, job):
+#     print(name, age, city, job)
+# print(person('Jack', 24, city='Beijing', job=''))
+
+# def person(name, age, *args, city='bj', job):
+#     print(name, age, args, city, job)
+# print(person('a', 24, job=''))
+# 区别在于，如果city给出默认值，输出的时候不输，不会报错，但是name会报错
+
+# def f1(a, b, c=0, *args, **kw):
+#     print('a=',a,'b=',b,'c=',c,'args=',args,'kw=',kw)
+
+# def f2(a, b, c=0, *, d, **kw):
+#     print('a=',a,'b=',b,'c=',c,'d=',d,'kw=',kw)
+# print(f1(1,2))
+# print(f1(1,2,3))
+# print(f1(1,2,3,'a','b'))
+# print(f1(1,2,3,'a','b',x=99))
+# print(f2(1,2,d='a',kw='b'))
+
+# args = (1,2,3,4)
+# args1 = (1,2,3)
+# kw = {'d':88,'x':'#'}
+# f1(*args,**kw)
+# f2(*args1,**kw)
+
+def product(m):
+    m = (x, **kw)
+    for n in m
+        sum = sum + n*n
+    return sum
+print(product(4))
