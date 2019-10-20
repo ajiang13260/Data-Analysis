1b0c982e0d86f6df6848f23e17176e4ea892c03c
ajiang <47351671+ajiang13260@users.noreply.github.com>
2019-10-20 15:36:34 +0800

    Update hellolxf.py
    递归函数学完！拜拜汉诺塔！

--- hellolxf.py
+++ hellolxf.py
@@ -470,9 +470,28 @@ y = 'ABC'
 #     except TypeError:
 #         print('测试成功！') 
 
-def fun(n):
-    if n == 1:
-        return 1
-    return n*fun(n-1)
+# def fun(n):
+#     if n == 1:
+#         return 1
+#     return n*fun(n-1)
 
-print(fun(3))
+# print(fun(3))
+
+# def fact(n):
+#     return fact_iter(n, 1)
+# def fact_iter(num, product):
+#     if num == 1:
+#         return product
+#     return fact_iter(num - 1, num * product)
+
+# print(fact(10))
+
+def move(n, a, b, c):
+    if n == 1:
+        print(a, '-->', c)
+        
+    else:
+        move(n-1,a,c,b)
+        move(1,a,b,c)
+        move(n-1,b,a,c)
+print(move(64, 'a', 'b', 'c'))
