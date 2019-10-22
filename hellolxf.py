f5649aadf789ce48287d13f0c7668067a59a125c
ajiang <47351671+ajiang13260@users.noreply.github.com>
2019-10-22 18:55:36 +0800

    Update hellolxf.py
    生成器：死于杨辉三角

--- hellolxf.py
+++ hellolxf.py
@@ -631,18 +631,104 @@ y = 'ABC'
 # L = ['Hello','World','IBM','Apple']
 # print([s.lower() for s in L])
 
-L1 = ['Hello','World',18,'Apple',None]
-# L = []
-# for x in L1:
-#     if isinstance(x,str) == True:
-#         L.append(x)
-# L2 = [s.lower() for s in L]
+# L1 = ['Hello','World',18,'Apple',None]
+# # L = []
+# # for x in L1:
+# #     if isinstance(x,str) == True:
+# #         L.append(x)
+# # L2 = [s.lower() for s in L]
 
-L2 = [s.lower() for s in L1 if isinstance(s,str)== True]
+# L2 = [s.lower() for s in L1 if isinstance(s,str)== True]
+
+# # 测试
+# print(L2)
+# if L2 == ['hello','world','apple'] :
+#     print('测试通过')
+# else:
+#     print('测试失败')
+
+    # 生成器
+# L = [x * x for x in range(10)]
+# g = (x * x for x in range(10))
+# # print(L,g)
+# # while True:
+# #     print(next(g))
+# for n in g:
+#     print(n)
+
+def fib(max):
+    n,a,b=0,0,1
+    while n < max:
+        # print (b)
+        yield b
+        n = n+1
+        a,b = b,a+b
+    return 'done'
+
+# print(fib(6))
+
+# def odd():
+#     print('step1')
+#     yield 1
+#     print('step2')
+#     yield 2
+#     print('step3')
+#     yield 3
+# o = odd()
+# print(next(o))
+# print(next(o))
+# print(next(o))
+
+# for n in fib(6):
+#     print(n)
+
+# g = fib(6)
+# while True:
+#     try:
+#         x = next(g)
+#         print('g:',x)
+#     except StopIteration as e:
+#         print('General reeturn value:',e.value)
+#         break
+
+def triangless():
+
+
+# 期待输出:
+# [1]
+# [1, 1]
+# [1, 2, 1]
+# [1, 3, 3, 1]
+# [1, 4, 6, 4, 1]
+# [1, 5, 10, 10, 5, 1]
+# [1, 6, 15, 20, 15, 6, 1]
+# [1, 7, 21, 35, 35, 21, 7, 1]
+# [1, 8, 28, 56, 70, 56, 28, 8, 1]
+# [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
 
 # 测试
-print(L2)
-if L2 == ['hello','world','apple'] :
+n = 0
+results = []
+for t in triangless():
+    results.append(t)
+    n = n + 1
+    if n == 10:
+        break
+for t in results:
+    print(t)
+
+if results == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+    [1, 6, 15, 20, 15, 6, 1],
+    [1, 7, 21, 35, 35, 21, 7, 1],
+    [1, 8, 28, 56, 70, 56, 28, 8, 1],
+    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+]:
     print('测试通过')
 else:
     print('测试失败')
