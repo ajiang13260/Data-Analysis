fc3d5fd82e1255855332e6b16975637beeb0f5d6
ajiang <47351671+ajiang13260@users.noreply.github.com>
2019-11-10 13:45:09 +0800

    changename
    重新划分文件夹并重命名

---
+++ playwith/1.1 Introduction.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 11:05:43 2019
+
+@author: Speed
+"""
+
+# print('hello world')
+# a = 'what is your name'
+# print(a)
+
+# price = input('input the stock price of apple:')
+# print(price)
+# print(type(price))
+# price = int(input('input the stock price of apple:'))
+# print(price)
+# print(type(price))
+# price = eval(input('input the stock price of apple:'))
+# print(price)
+# print(type(price))
+
+# comment No.1
+# print('hello world') #commet No.2
+
+# car = 'stop'
+# signal = 'green'
+# # long statement
+# if signal == 'red' and\
+#     car == 'moving':
+#     car = 'stop'
+#     print(car)
+# elif signal == 'green' and\
+#     car == 'stop':
+#     car = 'moving'
+#     print(car)
+
+# x = 'Today'; y = 'is'; z = 'Monday'
+# print(x,y,z)
+
+# p = 3.14159
+# s = 'is a mathematic circular constant'
+# print(p,s)
+
+# identifier
+# PI = 3.14159
+# pi = 'one world'
+# print(PI,pi)
+
+# print(5**2)
+# print(10//3)
+# print(~(7-13))
+
+# expression
+# PI = 3.14159
+# r = 2
+# c_circle = 2*PI*r
+# print("The circle's circum is", c_circle)
+
+# identifier
+# PI = 3.14159
+# pi = PI
+# print(pi)
+# p = 3
+# q = 3
+# print(p == q)
+
+# Augmented assignment
+# m = 18
+# m %= 5
+# print(m)
+# m **= 3
+# print(m)
+
+# Assignment
+# x = 1; y = 2
+# print(x,y)
+# x,y = y,x
+# print(x,y)
+# temp = 3,4
+# m,r = temp
+# print(m)
+# print(r)
+
+print(-4.13e-3)
