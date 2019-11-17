07c539094ea1e360eed22595113b73886d34330d
ajiang <47351671+ajiang13260@users.noreply.github.com>
2019-11-17 21:14:36 +0800

    Update 2.9 excercises.py

--- playwith/2.9 excercises.py
+++ playwith/2.9 excercises.py
@@ -15,6 +15,32 @@
 #     print('please enter right number')
 
 # second question
-for c in range(0,301,20):
-    f = c * 9 / 5 + 32
-    print('c = ', c, 'f = ', f)
+# for c in range(0,301,20):
+#     f = c * 9 / 5 + 32
+#     print('c = ', c, 'f = ', f)
+
+# third question
+# x = int(input('please enter a number:'))
+# while x != 1:
+#     if x % 2 == 0:
+#         print(x,'/ 2 =',end=' ')
+#         x = x / 2
+#         print(x)
+#     else:
+#         print(x, '* 3 + 1 =', end=' ')
+#         x = x * 3 + 1
+#         print(x)
+
+# fourth question
+# n = eval(input('enter a number as n:'))
+# def 
+
+# six question
+y = eval(input('enter a number:'))
+for i in  
+if y % 37 == 0:
+    y = y % 10 * 100 + y // 10
+    if y % 37 == 0:
+        print(True) 
+    else:
+        print(False) 
