787f320565f4f6269fbcc1977635ee390cb4b34d
ajiang <47351671+ajiang13260@users.noreply.github.com>
2019-12-13 02:11:47 +0800

    3.5

--- playwith/3.5 list.py
+++ playwith/3.5 list.py
@@ -61,4 +61,12 @@ import os
 def insert_line(lines):
     lines.insert(0,"Blowin'in the wind\n")
     lines.insert(1,"Bob Dylan\n")
-    lines.append("1962 by Warner Bros.lnnc")
+    lines.append("1962 by Warner Bros.lnc.")
+    return ''.join(lines)
+
+with open('Blowing in the wind.txt','r+') as f:
+    lines = f.readlines()
+    string = insert_line(lines)
+    print(string)
+    f.seek(0)
+    f.write(string)
