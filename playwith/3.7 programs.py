1828fb4469799c950485867df83cd345a5ffa8d3
ajiang <47351671+ajiang13260@users.noreply.github.com>
2019-12-17 17:58:15 +0800

    Update 3.7 programs.py

--- playwith/3.7 programs.py
+++ playwith/3.7 programs.py
@@ -1,12 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 headers = {'User-Agent':"Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.83 Safari/535.11"}
-r = requests.get('https://book.douban.com/subject/1084336/comments/', headers = headers)
-print(r.status_code)
-# r.encoding='utf-8'
-# print(r.text)
-
-# 目前还无法解决UnicodeError的问题
-with open('littleprince.md','w') as f:
-    f.write(r.text)
+# r = requests.get('https://movie.douban.com/subject/25887288/comments?status=P', headers = headers)
