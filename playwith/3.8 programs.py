77a991b706a53530380b1f493b8ce8f6386b572b
ajiang <47351671+ajiang13260@users.noreply.github.com>
2020-01-12 23:13:50 +0800

    Update 3.8 programs.py

--- playwith/3.8 programs.py
+++ playwith/3.8 programs.py
@@ -1,6 +1,13 @@
-import requests
+# Question one
+
+# step 1, 爬取短评（翻页）
+# step 2, 获取前50个评分（忽略空值，正则表达式）
+# step 3, 计算平均值(把文字转换成数据)
+
+
+import requests,re,time
 from bs4 import BeautifulSoup
-import re
 
 headers = {'User-Agent':"Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.83 Safari/535.11"}
-# r = requests.get('https://movie.douban.com/subject/25887288/comments?status=P', headers = headers)
+r = requests.get('https://book.douban.com/subject/1084336/comments/', headers = headers)
+print(r.text)
