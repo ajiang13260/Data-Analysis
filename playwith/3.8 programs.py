3dd44287ed40136cd6c84a6dd8c7d4fd9954bd20
ajiang <47351671+ajiang13260@users.noreply.github.com>
2020-01-12 23:32:27 +0800

    Update 3.8 programs.py
    不是爬取了50条，待解决

--- playwith/3.8 programs.py
+++ playwith/3.8 programs.py
@@ -10,4 +10,14 @@ from bs4 import BeautifulSoup
 
 headers = {'User-Agent':"Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.83 Safari/535.11"}
 r = requests.get('https://book.douban.com/subject/1084336/comments/', headers = headers)
-print(r.text)
+# print(r.text)
+# 不会翻页
+soup = BeautifulSoup(r.text,'lxml')
+pattern = re.compile(' <span class="user-stars allstar(.*) rating"')
+p = re.findall(pattern, r.text)
+s = 0
+i = 0
+for star in p:
+    s += int(star)
+    i += 1
+print(s*2/i/10)
