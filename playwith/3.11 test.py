84dc3c16b335bd2a30e6482384a771a58cfa3f77
ajiang <47351671+ajiang13260@users.noreply.github.com>
2020-05-14 08:48:12 +0800

    summary

--- playwith/3.11 test.py
+++ playwith/3.11 test.py
@@ -48,14 +48,11 @@
 
 # nums = [num.strip() for num in nums]
 
+# # 这个地方不太明白，是什么函数形式
+# 模仿一下
 # lst = [1,2,3,4]
 # lst1 = [str(i) for i in lst]
-# print(lst1)
 
-
-# # 这个地方不太明白，是什么函数形式
-# # print(nums)
-# # print(type(nums))
 # setNums = set(nums)
 # print(setNums)
 # lst = [0] * 101
@@ -70,12 +67,47 @@
 #         print(i)
 
 # 3rd
-with open('article.txt','r') as f:
-    content = f.readlines()
-sentence = content[0].split(',','.','!','?','...')
-result = []
-for i in range(len(sentence)):
-    result.append(len(i))
-for j in result:
-    if j == max(result):
-        print(sentence[])
+# 出现问题
+# UnicodeDecodeError: 'gbk' codec can't decode byte 0x9d in position 610: incomplete multibyte sequence
+
+# 行不通，NameError: name 'reload' is not defined
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf8')
+
+# 解决方案：在with open()内加上enconding='UTF-8'
+# _*_ coding:utf-8 _*_
+# with open('article.txt',encoding='UTF-8') as f:
+#     data = f.read()
+# words = data.split()
+# lst = []
+# for word in words:
+#     if word[-3:] == '...':
+#         # print(word)
+#         word = word[:-3]
+#         # print(word)
+#         lst.append(word)
+#     if word[-1] in ',.?!':
+#         word = word[:-1]
+#         lst.append(word)
+#     # 为什么对结果没有影响啊
+#     # if word[0] == '@':
+#     #     word = word[1:]
+#     #     lst.append(word)
+# result = sorted(lst,key = len,reverse= True)
+# maxlen = len(result[0])
+# for word in set(result):
+#     n = len(word)
+#     if n == maxlen:
+#         print(word)    
+                
+
+# 帮一位网友测试了一下jieba模块
+# import jieba
+# text = 'I have a dream.'
+# print('/'.join(jieba.lcut(text)))
+
+# import math
+# print((0,2)  in zip(range(4), range(2,6)))
+# print(list('Life is short, you need Python.').count('is'))
+# print('{:10.3f}'.format(math.pi))
---
+++ playwith/3.12 exam.py
@@ -0,0 +1,15 @@
+
+# 1st
+def countchar(string):
+    lst = [chr(ord(a)+i) for i in range(0,52)]
+    for i in range(string):
+        for j in range(lst):
+            if string[i] == lst[j]:
+                
+            
+
+     return a list
+if __name__ == "__main__":
+     string = input()
+
+     print(countchar(string))
