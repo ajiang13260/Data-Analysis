1f0853b848e3e4e2432d106b2cfcc1f937e6eb96
ajiang <47351671+ajiang13260@users.noreply.github.com>
2020-05-27 00:02:46 +0800

    summary

---
+++ playwith/4.1 dictionary.py
@@ -0,0 +1,46 @@
+# pList = [(1,'first','yi'),(2,'second','er'),
+#         (3,'third','san'),(4,'forth','si'),(5,'fifth','wu')]
+# d = {}
+# for item in pList:
+#     # 用()不可以，用[]可以，是为什么呢？
+#     d[item[0]] = item[2]
+# print(d)
+
+# 4.2 测验题1
+system = {'ajiang':'0546'}
+def newusers():
+    userName = input('Enter a name:')
+    if userName in system:
+        userName = input('Enter a name:')
+    else:
+        system[userName] = input('password:')
+
+def oldusers():
+    userName = input('Enter your username:')
+    passWord = input('Enter your password:')
+    if passWord == system[userName]:  
+        print(userName, 'welcome back ')  
+    else:  
+        print('login incorrect')  
+ 
+def login():
+    option = '''
+        (N)ew User Login 
+        (O)ld User Login
+        (E)xit
+            """
+     Enter the option
+     '''
+    print(option)
+    order = input()
+    if order == 'N':
+        newusers()
+    elif order == 'O':
+        oldusers()
+    elif order == 'E':
+        quit
+    else:
+        print('Please enter a right letter.')
+ 
+if __name__ == '__main__':  
+     login()
---
+++ playwith/4.2 dictionaryTest.py
@@ -0,0 +1,33 @@
+lst = [1,4,5,6,7,8,9,10,11,12,13,15,18,19,20,21,29,34,54,65]
+def twonums_sum(n,lst):
+    flag = -1
+    lst.sort()
+    for i in range(len(lst)):
+        for j in range(len(lst)):
+            if i != j:
+                if lst[i] + lst[j] == n:
+                    flag = 1
+                    return i,j
+    if flag == -1:
+        return 'not found'
+        
+n = eval(input('Enter a number:'))
+print(twonums_sum(n,lst))
+
+
+# def twonums_sum(n,lst):
+#     d = {}
+#     for i in range(len(lst)):
+#         d[lst[i]] = i
+#     for i in range(len(lst)):
+#         if n - lst[i] in d:
+#             return i,d[n-lst[i]]
+#     return -1
+
+# lst = [1,4,5,6,7,8,9,10,11,12,13,15,18,19,20,21,29,34,54,65]
+# n = int(input())
+# result = twonums_sum(n,lst)
+# if result == -1:
+#     print('not found')
+# else:
+#     print(result)
