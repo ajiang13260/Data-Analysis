c2642fe93edfaeaf9c4ded5fc91c817ad4ff427e
ajiang <47351671+ajiang13260@users.noreply.github.com>
2020-06-09 08:50:09 +0800

    s

--- playwith/4.1 dictionary.py
+++ playwith/4.1 dictionary.py
@@ -7,40 +7,43 @@
 # print(d)
 
 # 4.2 测验题1
-system = {'ajiang':'0546'}
-def newusers():
-    userName = input('Enter a name:')
-    if userName in system:
-        userName = input('Enter a name:')
-    else:
-        system[userName] = input('password:')
+# system = {'ajiang':'0546'}
+# def newusers():
+#     userName = input('Enter a name:')
+#     if userName in system:
+#         userName = input('Enter a name:')
+#     else:
+#         system[userName] = input('password:')
 
-def oldusers():
-    userName = input('Enter your username:')
-    passWord = input('Enter your password:')
-    if passWord == system[userName]:  
-        print(userName, 'welcome back ')  
-    else:  
-        print('login incorrect')  
+# def oldusers():
+#     userName = input('Enter your username:')
+#     passWord = input('Enter your password:')
+#     if passWord == system[userName]:  
+#         print(userName, 'welcome back ')  
+#     else:  
+#         print('login incorrect')  
  
-def login():
-    option = '''
-        (N)ew User Login 
-        (O)ld User Login
-        (E)xit
-            """
-     Enter the option
-     '''
-    print(option)
-    order = input()
-    if order == 'N':
-        newusers()
-    elif order == 'O':
-        oldusers()
-    elif order == 'E':
-        quit
-    else:
-        print('Please enter a right letter.')
+# def login():
+#     option = '''
+#         (N)ew User Login 
+#         (O)ld User Login
+#         (E)xit
+#             """
+#      Enter the option
+#      '''
+#     print(option)
+#     order = input()
+#     if order == 'N':
+#         newusers()
+#     elif order == 'O':
+#         oldusers()
+#     elif order == 'E':
+#         quit
+#     else:
+#         print('Please enter a right letter.')
  
-if __name__ == '__main__':  
-     login()
+# if __name__ == '__main__':  
+#      login()
+
+a = [1,2,3]
+b = []
