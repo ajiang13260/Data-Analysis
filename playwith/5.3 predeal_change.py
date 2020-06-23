eda19e31ef7eec90aa01fe8883f122ab99b3b567
ajiang <47351671+ajiang13260@users.noreply.github.com>
2020-06-23 23:23:18 +0800

    Update 5.3 predeal_change.py

--- playwith/5.3 predeal_change.py
+++ playwith/5.3 predeal_change.py
@@ -1,14 +1,14 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# score = pd.read_excel('score.xlsx')
-# score.boxplot()
-# plt.show()
-
 import pandas as pd
-quotesdf_nan = pd.read_csv('quotesdf.csv',index_col='date')
+import matplotlib.pyplot as plt
+score = pd.read_excel('score.xlsx')
+score.boxplot()
+plt.show()
+
+# import pandas as pd
+# quotesdf_nan = pd.read_csv('quotesdf.csv',index_col='date')
+# # print(quotesdf_nan)
+# # print(quotesdf_nan.isnull())
+# # print(quotesdf_nan.dropna(how='any'))
+# # quotesdf_nan.fillna(quotesdf_nan.mean(),inplace = True)
+# quotesdf_nan
 # print(quotesdf_nan)
-# print(quotesdf_nan.isnull())
-# print(quotesdf_nan.dropna(how='any'))
-# quotesdf_nan.fillna(quotesdf_nan.mean(),inplace = True)
-quotesdf_nan
-print(quotesdf_nan)
