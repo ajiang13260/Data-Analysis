a21b83048d3e87500f46a81852436a5552ed8bf5
ajiang <47351671+ajiang13260@users.noreply.github.com>
2020-07-04 18:05:16 +0800

    summary

--- playwith/5.6 tushare.py
+++ playwith/5.6 tushare.py
@@ -2,9 +2,46 @@ import tushare as ts
 import numpy as np
 # 1
 df = ts.get_hist_data('600036',start='2019-01-01',end='2019-04-30')
+# print(df)
 df = df.iloc[:,:5]
-print(df)
-df_1 = df.sort_index(inplace = True)
-print(df_1)
+# print(df)
+df.sort_index(inplace = True)
+# print(df)
 
-# 需要借用办公室大屏...不想分屏了好累
+# 2
+# print(df[['high','low']])
+# print(df.loc['2019-01-01':'2019-01-31',['high','low']])
+
+# 3
+# volume_sorted = df.sort_values(by='volume')
+# print(volume_sorted)
+# min_day = volume_sorted.iloc[0,:]
+# print(min_day)
+# min_volume = min_day.volume
+# min_volume_date = min_day.name
+# print('the min volume of {} is at {}'.format(min_volume,min_volume_date) )
+# max_day = volume_sorted.iloc[-1,:]
+# print(max_day)
+# max_volume = max_day.volume
+# max_volume_date = max_day.name
+# print('the max volume of {} is at {}'.format(max_volume,max_volume_date) )
+
+# 4
+# print(df[df.volume > 100000])
+
+# 5 
+# print(len(df[df.close > df.open]))
+
+# 6
+# print(df.open.diff())
+# print(np.sign(np.diff(df.open)))
+
+# 7
+import matplotlib.pyplot as plt
+# df_new = df.loc['2019-01-01':"2019-01-31",['high','low']]
+# df_new.sort_index().plot()
+# plt.show()
+
+# 8 
+plt.scatter(df.close-df.open,df.volume)
+plt.show()
---
+++ playwith/5.7 titanic.py
@@ -0,0 +1,7 @@
+# import seaborn as sns
+import pandas as pd
+
+# titanic_df = sns.load_dataset('titanic')
+# print(titanic_df)
+# titanic_df.to_csv('titanic.csv')
+titanic_df = pd.read_csv('titanic.csv')
