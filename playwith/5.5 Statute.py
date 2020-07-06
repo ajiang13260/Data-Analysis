c36d3e93718d281add619bc6d417dc64be347512
ajiang <47351671+ajiang13260@users.noreply.github.com>
2020-07-06 20:26:46 +0800

    sum

---
+++ playwith/5.5 Statute.py
@@ -0,0 +1,13 @@
+# PCA
+# from sklearn.decomposition import PCA
+# from sklearn import preprocessing,datasets
+# boston = datasets.load_boston()
+# x = preprocessing.scale(boston.data)
+# # print(x)
+# pca = PCA(n_components=5)
+# # print(pca)
+# pca.fit(x)
+# # print(pca)
+# print(pca.explained_variance_ratio_)
+# # print(pca)
+
--- playwith/5.7 titanic.py
+++ playwith/5.7 titanic.py
@@ -5,3 +5,17 @@ import pandas as pd
 # print(titanic_df)
 # titanic_df.to_csv('titanic.csv')
 titanic_df = pd.read_csv('titanic.csv')
+# print(titanic_df)
+
+# import matplotlib.pyplot as plt
+# plt.bar(titanic_df['age'],titanic_df['pclass'])
+# plt.scatter(range(891),titanic_df['age'])
+# titanic_df.loc[:891,'age'].plot(kind='bar',color='g')
+# plt.hist(titanic_df['age'],bins=10)
+# titanic_df['survived'].value_counts().plot(kind='pie')
+# plt.show()
+
+# print(titanic_df.sample(n=10))
+# print(titanic_df.sample(frac=0.3))
+
+print(titanic_df.iloc[:,4].describe())
